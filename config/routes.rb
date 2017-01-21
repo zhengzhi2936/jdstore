8879d217fe436a29b292a2f558874b509f273832
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-21 10:07:34 +0800

    step1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
   root 'welcome#index'
+  namespace :admin do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
