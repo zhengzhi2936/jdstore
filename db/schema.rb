b8914a2e75ea1ede33962ed8c8d8f709107ed3c2
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-02-10 14:26:26 +0800

    step 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210052439) do
+ActiveRecord::Schema.define(version: 20170210060405) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -100,6 +100,15 @@ ActiveRecord::Schema.define(version: 20170210052439) do
     t.string   "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "product_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "product_name"
+    t.integer  "product_price"
+    t.integer  "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "products", force: :cascade do |t|
